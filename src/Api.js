a2fec23952de7b7ecc0b9fa1f9d9e9c6e40dfc2c
unknown <gitishbhusri@gmail.com>
2021-03-12 19:54:42 +0530

    Pokedex first commit

---
+++ src/Api.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+// export const getPoke =()=>{
+//     const url=`https://pokeapi.co/api/v2/pokemon`;
+
+//     return fetch(url)
+//     .then((response)=>response.json())
+//     .catch((error) => console.log("Error : ", error));
+
+
+// };
--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import { Card, CardContent, Typography } from '@material-ui/core';
+
+
 
 function App() {
+  const getPoke =()=>{
+    const url=`https://pokeapi.co/api/v2/pokemon`;
+
+    return fetch(url)
+    .then((response)=>response.json())
+    .catch((error) => console.log("Error : ", error));
+
+};
+
+const getmore = (uurl)=>{
+  const url={uurl};
+
+  return fetch(url)
+  .then((response)=>response.json())
+  .catch((error) => console.log("Error : ", error));
+
+};
+
+
+
+const MyCard = ({ pok }) => {
+  
+  const moreDetails=(url)=>{
+    getmore(url)
+    .then((data)=> console.log("Poke Data",data))
+    .catch((error)=>console.log(error));
+  }
+  
+  const getCard = () =>{
+
+  return (
+
+<h2 onClick={()=>{
+  moreDetails(pok.url)
+}} style={{color: "orange"}}>{pok.name}</h2>
+
+  );
+  };
+  return getCard();
+};
+
+//---------------------------------------------------------------------------------------
+
+const [poke, setPoke] = useState([]);
+
+//setPoke(data.results)
+useEffect(()=>{
+  getPoke()
+  .then((data) => setPoke(data.results))
+  .catch((error) => alert("NOT loading data"));
+}
+,[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 style={{color: "blue"}}>POKEDEX APPLICATION</h1>
+     <h2 style={{color: "red"}}>List of Poke Names is :</h2>
+     <h3>Click on Names to get more detail</h3>
+     <h4>Details are shown in console.</h4>
+    {
+     poke.map((pok)=>(
+       
+       <MyCard pok={pok} />
+     ))
+   };
+     
     </div>
   );
 }
